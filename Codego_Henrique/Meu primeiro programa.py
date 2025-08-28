9c128a63b4ecf386328de820eafdb172f5de43a7
Henrique da Cruz Lima <cruzhenrique263@gmail.com>
2025-08-28 15:50:32 -0300

    Códigos do curso python.

---
+++ Codego_Henrique/Meu primeiro programa.py
@@ -0,0 +1,3 @@
+nome = input("Qual é o seu nome ? ")
+idade = input(f"Quantos anos vc tem {nome} ? ")
+print (f"{idade} anos ")
---
+++ Codego_Henrique/Meu segundo programa.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+#exemplo de variavel do tipo texto:
+
+#fruta = "morango"
+
+#print(type(fruta))
+
+#Exemplo Várial do tipo INT é inteiro, números inteiro 
+#idade = int (35)
+# Exemplo de Número quebrado, float
+#nota = float(9.5)
+#boleana 
+#vc_curte = False 
+
+### Exercicio 1 
+
+# criar uma varialvel que peça o nome do usuário
+# Depois criar uma variavel do tipo float, que pde a nota do usuario
+# depois criar uma variavel que peça a idade do usuário 
+# depois de um print nas variaveis
+
+
+nome_carro = ("i8")
+print(nome_carro)
+preço = float("710.950,00")
+print(preço)
+ano = int("2025")
+print(ano)
+
